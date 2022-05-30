f447ae7b95de2ee770475d7f72a048c36a37828d
lvcg <flowfxdesignsonline@gmail.com>
2022-05-30 15:08:37 -0500

    this is the commit message

---
+++ public/main.js
@@ -0,0 +1,40 @@
+const update = document.querySelector('#update-button')
+const deleteButton = document.querySelector('#delete-button')
+const messageDiv = document.querySelector('#message')
+
+update.addEventListener('click', () => {
+    fetch('/quotes', {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: 'Darth Vader',
+            quote: 'I find your lack of faith disturbing.'
+        })
+    })
+        .then(res => {
+            if (res.ok) return res.json()
+        })
+        .then(response => {
+            window.location.reload(true)
+        })
+})
+
+deleteButton.addEventListener('click', () => {
+    fetch('/quotes', {
+        method: 'delete',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: 'Darth Vader',
+        })
+    })
+        .then(res => {
+            if (res.ok) return res.json()
+        })
+        .then(response => {
+            if (response === 'No quote deleted') {
+                messageDiv.textContent = 'No quote deleted'
+            } else {
+            window.location.reload(true)
+            }
+        })
+})
